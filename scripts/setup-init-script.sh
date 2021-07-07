f1dcb019a26b1e668324f1fbbf9ccda11a50a955
Sean Sundberg <seansund@us.ibm.com>
2021-07-06 22:05:19 -0500

    Changes sed args
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/setup-init-script.sh
+++ scripts/setup-init-script.sh
@@ -13,10 +13,10 @@ SOURCE_FILE="${MODULE_DIR}/scripts/init-argocd.sh"
 set -x
 
 cat "${SOURCE_FILE}" | \
-  sed "s/ENV_IBMCLOUD_API_KEY/${IBMCLOUD_API_KEY}/g" | \
-  sed "s/ENV_SERVER_URL/${SERVER_URL}/g" | \
-  sed "s/ENV_CONFIG_REPO_URL/${CONFIG_REPO_URL}/g" | \
-  sed "s/ENV_CONFIG_USERNAME/${CONFIG_USERNAME}/g" | \
-  sed "s/ENV_CONFIG_TOKEN/${CONFIG_TOKEN}/g" | \
-  sed "s/ENV_BOOTSTRAP_PATH/${BOOTSTRAP_PATH}/g" \
+  sed "s~ENV_IBMCLOUD_API_KEY~${IBMCLOUD_API_KEY}~g" | \
+  sed "s~ENV_SERVER_URL~${SERVER_URL}~g" | \
+  sed "s~ENV_CONFIG_REPO_URL~${CONFIG_REPO_URL}~g" | \
+  sed "s~ENV_CONFIG_USERNAME~${CONFIG_USERNAME}~g" | \
+  sed "s~ENV_CONFIG_TOKEN~${CONFIG_TOKEN}~g" | \
+  sed "s~ENV_BOOTSTRAP_PATH~${BOOTSTRAP_PATH}~g" \
   > "${OUTPUT_FILE}"
