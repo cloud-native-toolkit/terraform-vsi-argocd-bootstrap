06e9fdb4d726e4f45ed7dc86a14b9cac222e7791
Sean Sundberg <seansund@us.ibm.com>
2021-07-09 22:20:43 -0500

    Updates init script
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/init-script.sh
+++ scripts/init-script.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Disable password authentication
+# Whether commented or not, make sure they are uncommented and explicitly set to 'no'
+grep -q "ChallengeResponseAuthentication" /etc/ssh/sshd_config && sed -i "/^[#]*ChallengeResponseAuthentication[[:space:]]yes.*/c\ChallengeResponseAuthentication no" /etc/ssh/sshd_config || echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+grep -q "PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config || echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+# If any other files are Included, comment out the Include
+# Sometimes IBM stock images have an uppercase Include like this.
+sed -i "s/^Include/# Include/g" /etc/ssh/sshd_config
+
+service ssh restart
+
+# As a precaution, delete the root password in case it exists
+passwd -d root
+
+apt-get -y update
+apt-get -y upgrade
+
 # install oc cli
 OPENSHIFT_CLI_VERSION="4.7.2"
 HELM_VERSION="3.6.2"
