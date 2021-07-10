909bfb1997925845f2dd7170d91a1a5912d1e03e
Sean Sundberg <seansund@us.ibm.com>
2021-07-10 00:34:19 -0500

    Updates validate deploy script
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -9,6 +9,8 @@ else
 fi
 export KUBECONFIG
 
+oc get job -n default
+
 if ! oc get job ibm-toolkit-install -n default 1> /dev/null 2> /dev/null; then
   echo "ibm-toolkit-install job not created"
   exit 1
