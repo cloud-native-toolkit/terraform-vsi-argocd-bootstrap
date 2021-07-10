d325fd10fd47209ef5b6958b3c5fbbb03cf3adfc
Sean Sundberg <seansund@us.ibm.com>
2021-07-10 00:28:19 -0500

    Updates script
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/init-argocd.sh
+++ scripts/init-argocd.sh
@@ -7,6 +7,29 @@ GITOPS_CONFIG_USERNAME="ENV_CONFIG_USERNAME"
 GITOPS_CONFIG_TOKEN="ENV_CONFIG_TOKEN"
 GITOPS_BOOTSTRAP_PATH="ENV_BOOTSTRAP_PATH"
 
+# install oc cli
+OPENSHIFT_CLI_VERSION="4.7.2"
+HELM_VERSION="3.6.2"
+
+curl -sL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OPENSHIFT_CLI_VERSION}/openshift-client-linux.tar.gz --output oc-client.tar.gz && \
+    mkdir tmp && \
+    cd tmp && \
+    tar xzf ../oc-client.tar.gz && \
+    mv ./oc /usr/local/bin/oc && \
+    chmod +x /usr/local/bin/oc && \
+    cd .. && \
+    rm -rf ./tmp && \
+    rm oc-client.tar.gz
+
+curl -sL https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz -o helm.tar.gz && \
+    mkdir tmp && \
+    cd tmp && \
+    tar xzf ../helm.tar.gz && \
+    mv linux-amd64/helm /usr/local/bin/helm &&
+    cd .. && \
+    rm -rf ./tmp &&
+    rm helm.tar.gz
+
 # login to cluster
 oc login -u apikey -p $IBMCLOUD_API_KEY $SERVER_URL
 
