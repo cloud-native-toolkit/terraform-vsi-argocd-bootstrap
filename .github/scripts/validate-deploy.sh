08575ffb1e470127219fa2ada2f34cce58bc8767
Sean Sundberg <seansund@us.ibm.com>
2021-08-25 23:32:48 -0500

    Updates validation logic to test for deployments
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -9,9 +9,33 @@ else
 fi
 export KUBECONFIG
 
+NAMESPACE=$(cat .namespace)
+
 oc get job -n default
 
 if ! oc get job ibm-toolkit-install -n default 1> /dev/null 2> /dev/null; then
   echo "ibm-toolkit-install job not created"
   exit 1
 fi
+
+oc get all -n "${NAMESPACE}"
+
+IN="artifactory;sonarqube;dashboard"
+
+IFS=';' read -ra DEPLOYMENTS <<< "$IN"
+for deployment in "${DEPLOYMENTS[@]}"; do
+  # process "$i"
+  count=0
+  until kubectl get deployment "${deployment}" -n "${NAMESPACE}" 1> /dev/null 2> /dev/null || [[ $count -eq 20 ]]; do
+    echo "Waiting for deployment: ${deployment}"
+    count=$((count + 1))
+    sleep 30
+  done
+
+  if [[ $count -eq 20 ]]; then
+    echo "Timed out waiting for deployment to start: ${deployment}"
+    exit 1
+  fi
+
+  kubectl rollout status deployment "${deployment}" -n "${NAMESPACE}"
+done
--- test/stages/stage3-namespace.tf
+++ test/stages/stage3-namespace.tf
@@ -8,3 +8,9 @@ module "gitops_namespace" {
   argocd_namespace = module.argocd-bootstrap.argocd_namespace
   argocd_service_account = module.argocd-bootstrap.argocd_service_account
 }
+
+resource null_resource print_namespace {
+  provisioner "local-exec" {
+    command = "echo '${module.gitops_namespace.name}' > .namespace"
+  }
+}
