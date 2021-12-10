ee2bae45af799ffec8bd5d20aa668b3b4085824f
Sean Sundberg <seansund@us.ibm.com>
2021-12-10 15:28:04 -0600

    Upgrades argocd bootstrap version
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- terraform/stages.tf
+++ terraform/stages.tf
@@ -17,7 +17,7 @@ module "dev_software_olm" {
 }
 
 module "argocd-bootstrap" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-argocd-bootstrap.git?ref=v1.4.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-argocd-bootstrap.git?ref=v1.4.2"
 
   cluster_type        = module.cluster.platform.type_code
   ingress_subdomain   = module.cluster.platform.ingress
