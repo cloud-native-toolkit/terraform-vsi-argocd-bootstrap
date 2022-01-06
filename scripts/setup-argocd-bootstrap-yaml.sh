843bb94437fe0c7327eb62ae3f75566eccd77e9f
Sean Sundberg <seansund@us.ibm.com>
2022-01-06 16:14:26 -0600

    Updates setup-argocd-bootstrap-yaml script
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/setup-argocd-bootstrap-yaml.sh
+++ scripts/setup-argocd-bootstrap-yaml.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set +x
+
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
@@ -13,13 +15,13 @@ mkdir -p "${OUTPUT_DIR}"
   ibm-toolkit-install \
   --repo https://charts.cloudnativetoolkit.dev \
   --namespace default \
-  --set config.gitops_config_repo_url=$GITOPS_CONFIG_REPO_URL \
-  --set config.gitops_config_username=$GITOPS_CONFIG_USERNAME \
-  --set config.gitops_config_token=$GITOPS_CONFIG_TOKEN \
-  --set config.gitops_bootstrap_path=$GITOPS_BOOTSTRAP_PATH \
-  --set config.ingress_subdomain=$INGRESS_SUBDOMAIN \
-  --set config.sealed_secret_cert=$SEALED_SECRET_CERT \
-  --set config.sealed_secret_private_key=$SEALED_SECRET_PRIVATE_KEY \
-  --set repo.url=https://github.com/cloud-native-toolkit/terraform-vsi-argocd-bootstrap \
-  --set repo.branch=$BOOTSTRAP_BRANCH \
+  --set "config.gitops_config_repo_url=${GITOPS_CONFIG_REPO_URL}" \
+  --set "config.gitops_config_username=${GITOPS_CONFIG_USERNAME}" \
+  --set "config.gitops_config_token=${GITOPS_CONFIG_TOKEN}" \
+  --set "config.gitops_bootstrap_path=${GITOPS_BOOTSTRAP_PATH}" \
+  --set "config.ingress_subdomain=${INGRESS_SUBDOMAIN}" \
+  --set "config.sealed_secret_cert=${SEALED_SECRET_CERT}" \
+  --set "config.sealed_secret_private_key=${SEALED_SECRET_PRIVATE_KEY}" \
+  --set "repo.url=https://github.com/cloud-native-toolkit/terraform-vsi-argocd-bootstrap" \
+  --set "repo.branch=${BOOTSTRAP_BRANCH}" \
   --set repo.path=terraform > "${OUTPUT_FILE}"
